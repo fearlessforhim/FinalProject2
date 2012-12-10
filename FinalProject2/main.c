7722310e22dab4d4c824459e86e6460c9442eddf
fearlessforhim <jonathan.cameron6@gmail.com>
2012-12-10 13:14:01 -0700

    Initial Commit

---
+++ FinalProject2/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  FinalProject2
+//
+//  Created by Jonathan Cameron on 12/10/12.
+//  Copyright (c) 2012 Jonathan Cameron. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
